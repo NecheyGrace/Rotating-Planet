a29e7befdb4fa131acb0b1d40f9a333847addc8e
Amara Nechey <37805319+NecheyGrace@users.noreply.github.com>
2022-09-11 07:43:19 -0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+//You are given an array of integers numbers and two integers left and right. You task is to calculate a boolean array result, where result[i] = true if there exists an integer x, such that numbers[i] = (i + 1) * x and left ≤ x ≤ right. Otherwise, result[i] should be set to false.
+function solution(numbers, left, right) {
+  //
+}
